5d00b71e5c53af3cbfb3098ae69d2827a51265a2
Clemens Hutter <ch.23@web.de>
2020-12-18 17:46:15 +0100

    WIP teacher auth works

---
+++ klassentrener-ui/src/app/_services/storage.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class StorageService {
+
+  constructor() { }
+
+
+  store(key:string, value: string){
+    
+  }
+}
