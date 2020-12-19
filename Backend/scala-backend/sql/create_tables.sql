fcadfa58896b955aa50c821658471eb770e1eaba
Anton Laukemper <github@laukemper.it>
2020-12-19 19:21:12 +0100

    add table creation sql code

---
+++ Backend/scala-backend/sql/create_tables.sql
@@ -0,0 +1,26 @@
+CREATE TABLE SchoolClass (
+    id serial PRIMARY KEY,
+    className varchar(20) NOT NULL,
+    schoolName varchar(30),
+    classSecret varchar(100) NOT NULL,
+    encryptedPublicKey varchar(100) NOT NULL,
+    teacherSecret varchar(100) NOT NULL,
+    privateKey varchar(100) NOT NULL,
+    surveyStatus varchar()
+);
+
+CREATE TABLE Student (
+    id serial PRIMARY KEY,
+    FOREIGN KEY (classId) REFERENCES SchoolClass (id) ON DELETE CASCADE,
+    hash varchar(100) NOT NULL,
+    encryptedName varchar(100) NOT NULL,
+    selfReported boolean NOT NULL,
+    groupBelonging int DEFAULT NULL
+);
+
+CREATE TABLE Relationship (
+    id serial PRIMARY KEY,
+    FOREIGN KEY (sourceID) REFERENCES SchoolClass (id) ON DELETE SET NULL,
+    FOREIGN KEY (targetID)  REFERENCES SchoolClass(id) ON DELETE SET NULL
+ 
+);
