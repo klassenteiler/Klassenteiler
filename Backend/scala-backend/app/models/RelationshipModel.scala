569f9b82233dd3d249f35951518215fb90281e38
Anton Laukemper <github@laukemper.it>
2021-01-31 23:20:09 +0100

    fix bug in method

--- Backend/scala-backend/app/models/RelationshipModel.scala
+++ Backend/scala-backend/app/models/RelationshipModel.scala
@@ -32,7 +32,18 @@ class RelationshipModel(db: Database)(implicit ec: ExecutionContext) {
 
   // the target of all relations where target is oldId are replaced by newId
   def rewireRelations(oldId: Int, newId: Int): Future[Boolean] = {
-    val affectedRows: Future[Int] = db.run(Relationship.filter(_.targetid === oldId).map(row => (row.targetid)).update(Some(newId)))
-    affectedRows.map(rows => rows > 0)
+    // first we need to check whether students with that integer exist or else our database complains
+    val students: Future[Seq[StudentRow]] = db.run(Student.filter(x => (x.id === oldId || x.id === newId)).result)
+    students.flatMap(rows =>{
+      if(rows.length != 2){
+        Future.successful(false)
+      }else{
+        val affectedRows: Future[Int] = db.run(Relationship.filter(_.targetid === oldId).map(row => (row.targetid)).update(Some(newId)))
+        affectedRows.map(rows => rows > 0)
+
+      }
+    })
+
+
   }
 }
